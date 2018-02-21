c442e2de92173f31fa43f88808d2b40b5d707e22
hrhodus <31553581+hrhodus@users.noreply.github.com>
2018-02-21 13:45:19 -0500

    Update Maze.java

--- src/maze/Maze.java
+++ src/maze/Maze.java
@@ -1,5 +1,7 @@
 package maze;
 
+import java.util.Scanner;
+
 /**
  * Created by Tulin Kilinc 1/19/2017.
  * KSU Programming Principles II
@@ -11,47 +13,123 @@ public class Maze {
     private int r;  // x position of the mouse
     private int c;  //y position of the mouse
     private boolean exitFound = false;
+    int[][] maze;
 
 
 
 
     public Maze(int[][] arrMaze) {
+        this.maze=arrMaze;
+        r=maze.length-1;
+        c=0;
      }
 
     //Prints out the maze without solution
     public void displayMaze() {
+        for (int i=0; i< maze.length; i++) {
+
+            for (int n = 0; n<maze[i].length; n++){
+                if (maze[i][n]==0){
+                    System.out.print('#');
+                }
+                else{System.out.print(" ");}
+            }
+            System.out.println();
+        }
 
     }
 
     //displays the Maze with the path taken
     public void displayPath() {
+        for (int i=0; i< maze.length; i++) {
+
+            for (int n = 0; n<maze[i].length; n++){
+                if (i==r && n==c){
+                    System.out.print('@');
+                }
+                else if(maze[i][n]==3){
+                    System.out.print('~');
+                }
+                else if(maze[i][n]==1) {
+                    System.out.print(" ");
+                }
+                else {System.out.print("#");}
+            }
+            System.out.println();
+        }
 
     }
 
 
     public boolean takeStep() {
-        //complete the code here
+        Scanner scan = new Scanner(System.in);
+        System.out.println("would you like to go up, down, left or right?");
+        String direction= scan.nextLine();
 
+        if (direction.equalsIgnoreCase("up")){
+            moveNorth();
+        }
+        else if (direction.equalsIgnoreCase("down")){
+            moveSouth();
+        }
+        else if (direction.equalsIgnoreCase("left")){
+            moveWest();
+        }
+        else if (direction.equalsIgnoreCase("right")){
+            moveEast();
+        }
+        else {
+            System.out.println("Direction does not exist. Please try again");
+            takeStep();
+        }
+        displayPath();
         return isAnExit();
     }
 
     private void moveNorth() {
-        //complete the code here
+        if (r-1<0){
+            System.out.println("cannot move any further in this direction\n please try again");
+            takeStep();
+        }
+        else if(maze[r-1][c]!= 0){
+            maze[r][c] =3;
+            r--;
+        }
 
     }
 
     private void moveSouth() {
-        //complete the code here
-
+        if (r+1>maze.length-1){
+            System.out.println("cannot move any further in this direction\nplease try again");
+            takeStep();
+        }
+        else if(maze[r+1][c]!= 0){
+            maze[r][c] =3;
+            r++;
+        }
     }
 
     private void moveEast() {
-        //complete the code here
+        if(c+1>maze[r].length){
+            System.out.println("cannot move any further in this direction\nplease try again");
+            takeStep();
+        }
+        else if(maze[r][c+1]!= 0){
+            maze[r][c] =3;
+            c++;
+        }
 
     }
 
     private void moveWest() {
-        //complete the code here
+        if(c-1<0){
+            System.out.println("cannot move any further in this direction\nplease try again");
+            takeStep();
+        }
+        else if(maze[r][c-1]!= 0){
+            maze[r][c] =3;
+            c--;
+        }
 
     }
 
