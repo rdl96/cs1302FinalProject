452b688c64489396bb2b36f6e402193d8ba6b568
hrhodus <31553581+hrhodus@users.noreply.github.com>
2018-04-17 11:37:52 -0400

    Update Player.java

--- src/Pishti/Player.java
+++ src/Pishti/Player.java
@@ -7,16 +7,8 @@ public class Player implements Game {
     private  int score = 0;
     private ArrayList<Integer> currentHand= new ArrayList<>();
 
-    public Player(ArrayList cardsDealt){
+    public Player(){
         score = 0;
-        for(int i =0; i<4;i++){
-            int cardNum = (int) (Math.random()*52);
-
-            if(!currentHand.contains(cardNum)&&!cardsDealt.contains(cardNum)) {
-                currentHand.add(cardNum);
-            }
-            else{i--;}
-        }
     }
     @Override
     public void newHand(ArrayList cardsDealt) {
