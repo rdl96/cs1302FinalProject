fc2a3d415b7b4bdcc431e73a46ba7597d7fc4865
hrhodus <31553581+hrhodus@users.noreply.github.com>
2018-04-17 11:36:11 -0400

    Update Ai.java

--- src/Pishti/Ai.java
+++ src/Pishti/Ai.java
@@ -3,12 +3,59 @@ package Pishti;
 import java.util.ArrayList;
 
 public class Ai implements Game {
+    private int score;
+    private ArrayList<Integer> currentHand= new ArrayList<>();
+    private ArrayList<Integer> alreadyPlayed = new ArrayList<>();
+    private int currentHandSize = 3;
+
+    public Ai (){
+        score = 0;
+    }
     public void newHand(ArrayList cardsDealt){
-        return;
+        {
+
+            for(int i =0; i<4;i++){
+                int cardNum = (int) (Math.random()*52);
+
+                if(!currentHand.contains(cardNum)&&!cardsDealt.contains(cardNum)) {
+                    currentHand.add(cardNum);
+                }
+                else{i--;}
+            }
+        }
     }
 
     @Override
     public ArrayList getHand() {
-        return null;
+        return currentHand;
     }
+
+
+    public int turn(int[] cardnum, int currentCard){
+        if (currentHandSize == 0){
+            currentHandSize = 3;
+        }
+        int card = (int) Math.random()*currentHandSize;
+        Boolean turner = false;
+
+        for (int i= 0; i < currentHandSize; i++){
+            if (currentCard == cardnum[currentHand.get(i)]){
+                return cardnum[currentHand.get(i)];
+            }
+        }
+
+        while (!turner) {
+            if ((cardnum[currentHand.get(card)] == 11 && currentHand.size() > 1)) {
+                card = (int) Math.random() *currentHandSize;
+                turner = false;
+            }
+            else{
+                currentHandSize--;
+                alreadyPlayed.add(currentHand.get(card));
+                turner = true;
+            }
+        }
+        return card;
+    }
+
 }
